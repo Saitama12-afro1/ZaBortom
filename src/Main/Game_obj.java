886f62607f8df25ae234abe4d25020b6f33a90f2
NectoT <nectot2@gmail.com>
2020-12-21 15:23:44 +0300

    Классы персонажей больше не подклассы GamePerson; Добавлен тестовый метод run
    А ещё пофикшен размер массива location_ship

--- src/Main/Game_obj.java
+++ src/Main/Game_obj.java
@@ -6,7 +6,25 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Game_obj {
-    public GamePerson [] location_ship=new GamePerson[5];
+
+    public void run() { // просто функция для тестов штук
+        ArrayList<GamePerson> persons = new ArrayList<>();
+
+        persons.add(new Ledy());
+        persons.add(new Capitalist());
+        persons.add(new Capitan());
+        persons.add(new Boatswain());
+        persons.add(new Frenchie());
+        persons.add(new Kid());
+        try {
+            new NavigationDeck(persons);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public GamePerson [] location_ship=new GamePerson[6];
     public class GamePerson
     {
         public  String name_person;
@@ -41,96 +59,93 @@ public class Game_obj {
               return  new Item("f");
 
           }
-          class Kid extends GamePerson
-          {
-              Kid()
-              {
-                  super("Kid");
-                  size_life=3;
-                  size_bonuslife=9;
-                  location_ship[5]=this;
-              }
-              public ArrayList<Item> thievery(GamePerson name_enemy)
-              {
-                  Random random=new Random();
-                  int i= random.nextInt(name_enemy.count_items);
-                   this.items_person[count_items++]=name_enemy.items_person[i];//настроить  работу с полем видимости
-                  name_enemy.items_person[i].remove(i);
-                   return  this.items_person[count_items];
-              }
-
-          }
-         class Frenchie extends GamePerson
-         {
-             Frenchie()
-             {
-                 super("Frenchie");
-                 size_life=6;
-                 size_bonuslife=6;
-                 location_ship[4]=this;
-             }
-            public void ignor_damage_sea()
-            {
-                damage_in_sea=0;
-            }
-
-         }
-         class Boatswain extends GamePerson
-         {
-             Boatswain()
-             {
-                 super("Boatswain");
-                 size_life=8;
-                 size_bonuslife=4;
-                 location_ship[3]=this;
-             }
-         }
-         class Capitan extends GamePerson
-         {
-             Capitan()
-             {
-                 super("Capitan");
-                 size_life=7;
-                 size_bonuslife=5;
-                 location_ship[2]=this;
-             }
-             public int bonus_many()
-             {
-                 return 1;
-             }
-         }
-         class Capitalist extends GamePerson
-         {
-             Capitalist()
-             {
-                 super("Capitalist");
-                 size_life=5;
-                 size_bonuslife=7;
-                 location_ship[1]=this;
-             }
-             public int bonus_paintings()
-             {
-                 return 1;
-             }
-         }
-         class Ledy extends GamePerson
-         {
-             Ledy()
-             {
-                 super("Ledy");
-                 size_life=4;
-                 size_bonuslife=8;
-                 location_ship[0]=this;
-             }
-             public int bonus_jewelry()
-             {
-                 return 1;
-             }
-         }
-
-
-
-
      }
+
+    public class Kid extends GamePerson
+    {
+        Kid()
+        {
+            super("Kid");
+            size_life=3;
+            size_bonuslife=9;
+            location_ship[5]=this;
+        }
+        public ArrayList<Item> thievery(GamePerson name_enemy)
+        {
+            Random random=new Random();
+            int i= random.nextInt(name_enemy.count_items);
+            this.items_person[count_items++]=name_enemy.items_person[i];//настроить  работу с полем видимости
+            name_enemy.items_person[i].remove(i);
+            return  this.items_person[count_items];
+        }
+
+    }
+    public class Frenchie extends GamePerson
+    {
+        Frenchie()
+        {
+            super("Frenchie");
+            size_life=6;
+            size_bonuslife=6;
+            location_ship[4]=this;
+        }
+        public void ignor_damage_sea()
+        {
+            damage_in_sea=0;
+        }
+
+    }
+    public class Boatswain extends GamePerson
+    {
+        Boatswain()
+        {
+            super("Boatswain");
+            size_life=8;
+            size_bonuslife=4;
+            location_ship[3]=this;
+        }
+    }
+    public class Capitan extends GamePerson
+    {
+        Capitan()
+        {
+            super("Capitan");
+            size_life=7;
+            size_bonuslife=5;
+            location_ship[2]=this;
+        }
+        public int bonus_many()
+        {
+            return 1;
+        }
+    }
+    public class Capitalist extends GamePerson
+    {
+        Capitalist()
+        {
+            super("Capitalist");
+            size_life=5;
+            size_bonuslife=7;
+            location_ship[1]=this;
+        }
+        public int bonus_paintings()
+        {
+            return 1;
+        }
+    }
+    public class Ledy extends GamePerson
+    {
+        public Ledy()
+        {
+            super("Ledy");
+            size_life=4;
+            size_bonuslife=8;
+            location_ship[0]=this;
+        }
+        public int bonus_jewelry()
+        {
+            return 1;
+        }
+    }
 
 }
